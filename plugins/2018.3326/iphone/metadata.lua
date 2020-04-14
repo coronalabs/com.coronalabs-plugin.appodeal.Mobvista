3f09e4cfc86a63fa5c80f9306cc9a0bcc6ff9bab
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:36:28 -0400

    Adding binaries

---
+++ plugins/2018.3326/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDMintegralAdapter', },
+		frameworks = { 'MTGSDK', 'MTGSDKAppWall', 'MTGSDKBanner', 'MTGSDKBidding', 'MTGSDKInterActive', 'MTGSDKInterstitial', 'MTGSDKInterstitialVideo', 'MTGSDKOfferWall', 'MTGSDKReward', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
