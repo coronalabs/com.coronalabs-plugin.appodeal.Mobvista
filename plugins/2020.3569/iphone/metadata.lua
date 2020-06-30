058f5f029903798f2b79dc05f2df5b9d5a6ff3dc
Vlad Shcherban <vlad@coronalabs.com>
2020-06-30 10:57:12 +0300

    Updating binaries

---
+++ plugins/2020.3569/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDMintegralAdapter', },
+		frameworks = { 'MTGSDK', 'MTGSDKBanner', 'MTGSDKBidding', 'MTGSDKInterActive', 'MTGSDKInterstitial', 'MTGSDKInterstitialVideo', 'MTGSDKReward', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
