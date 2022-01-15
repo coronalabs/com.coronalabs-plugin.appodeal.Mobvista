4702bdbd9c52977c5f8c85b01136a264748be322
Scott Harrison <scottrules44@gmail.com>
2022-01-15 11:31:40 -0500

    not currently by supported by Appodeal replacing with stub

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'APDMintegralAdapter', },
-		frameworks = { 'MTGSDK', 'MTGSDKBanner', 'MTGSDKBidding', 'MTGSDKInterActive', 'MTGSDKInterstitial', 'MTGSDKInterstitialVideo', 'MTGSDKNativeAdvanced', 'MTGSDKReward', 'MTGSDKSplash', },
+		staticLibs = { },
+		frameworks = { },
 		frameworksOptional = {},
 	},
 }
